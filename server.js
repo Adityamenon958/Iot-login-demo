55eebdd945769261d93daadae1ab69c3c2eabc79
aditya <adityamenon958@gmail.com>
2025-05-08 21:50:47 +0530

    Deployment Azure 19

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ app.use(express.json());
 // DB connect
 connectDB();
 
-app.use(express.static(path.join(__dirname, "frontend/dist")));
 
 
 // API routes
@@ -78,6 +77,9 @@ app.delete('/api/devices/:id', async (req, res) => {
 });
 
 // ✅ Serve frontend from frontend/dist
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
