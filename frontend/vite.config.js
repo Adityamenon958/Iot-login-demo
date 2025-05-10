52e6556edc154923058d2400ef20cc585706c9d2
aditya <adityamenon958@gmail.com>
2025-05-10 09:07:12 +0530

    Deployment Azure 30

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // This should match your Express server port
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   
 })
